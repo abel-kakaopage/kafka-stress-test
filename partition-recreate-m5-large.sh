0de9dbd0819925571a8acf9d953c7ebf0bbd8981
abel <abel@kakaopage.com>
2021-05-23 01:05:35 +0900

    멀티 프로커 설정 추가

---
+++ partition-recreate-m5-large.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ZOOKEEPER=z-2.perf-test-m5-larg.vbxnpm.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-1.perf-test-m5-larg.vbxnpm.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-3.perf-test-m5-larg.vbxnpm.c2.kafka.ap-northeast-2.amazonaws.com:2181
+
+echo 'Delete Partition'
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-gzip-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-lz4-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-snappy-test
+echo 'Create Partition'
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions 1 --topic performance-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions 1 --config compression.type=gzip --topic performance-gzip-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions 1 --config compression.type=lz4 --topic performance-lz4-test
+/home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions 1 --config compression.type=snappy --topic performance-snappy-test
