1e4f63255633603c69bb4d5139eadb9e26104d55
abel <abel@kakaopage.com>
2021-05-25 00:28:39 +0900

    브로커 수정

--- perf-test-m5.large.sh
+++ perf-test-m5.large.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-KAFKA=bootstrap.servers=b-1.abel-perf-test.3ls28a.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-2.abel-perf-test.3ls28a.c2.kafka.ap-northeast-2.amazonaws.com:9092
-ZOOKEEPER=z-1.abel-perf-test.3ls28a.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-3.abel-perf-test.3ls28a.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-2.abel-perf-test.3ls28a.c2.kafka.ap-northeast-2.amazonaws.com:2181
+KAFKA=bootstrap.servers=b-1.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-2.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:9092
+ZOOKEEPER=z-1.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-3.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-2.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181
 INSTANCE=m5.large
 INDEX=$(seq 1 4)
 for i in $INDEX
 do
     PARTITION=partition-$i
     echo $PARTITION
-#
-#    echo 'Delete Partition'
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-gzip-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-lz4-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-snappy-test
-#    echo 'Create Partition'
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --topic performance-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=gzip --topic performance-gzip-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=lz4 --topic performance-lz4-test
-#    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=snappy --topic performance-snappy-test
+
+    echo 'Delete Partition'
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-gzip-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-lz4-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --delete --zookeeper $ZOOKEEPER --topic performance-snappy-test
+    echo 'Create Partition'
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --topic performance-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=gzip --topic performance-gzip-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=lz4 --topic performance-lz4-test
+    /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 2 --partitions $i --config compression.type=snappy --topic performance-snappy-test
 
     echo ${INSTANCE}-1
     /home/ec2-user/kafka_2.12-2.2.1/bin/kafka-producer-perf-test.sh --num-records 1000000 --throughput -1 --record-size 1024 --topic performance-test --print-metrics --producer-props $KAFKA > kafka.${INSTANCE}.${PARTITION}_1.txt
