7bd183f28f268b9391c567e4204dcb5fa4d1bf34
abel <abel@kakaopage.com>
2021-05-25 01:17:36 +0900

    브로커 수정 2broker 2partition

--- perf-test-m5.large-multi.sh
+++ perf-test-m5.large-multi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-KAFKA=bootstrap.servers=b-1.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-2.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:9092
-ZOOKEEPER=z-1.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-3.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-2.perf-test-m5-larg.53fxw0.c2.kafka.ap-northeast-2.amazonaws.com:2181
+KAFKA=bootstrap.servers=b-2.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-1.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-4.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-3.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:9092
+ZOOKEEPER=z-3.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-1.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:2181,z-2.perf-test-m5-larg.wwiqv1.c2.kafka.ap-northeast-2.amazonaws.com:2181
 INSTANCE=m5.large
 PAYLOAD=payload.txt
 INDEX=$(seq 1 1)
